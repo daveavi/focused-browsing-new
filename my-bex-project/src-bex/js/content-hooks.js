5e1175bd2edb1f4a9ebb94514cab4e7f13c8f3c6
daveavi <avidave97@gmail.com>
2021-04-26 19:05:52 -0400

    works very smoothly keyboard short cut

--- my-bex-project/src-bex/js/content-hooks.js
+++ my-bex-project/src-bex/js/content-hooks.js
@@ -1,22 +1,28 @@
 // Hooks added here have a bridge allowing communication between the BEX Content Script and the Quasar Application.
 // More info: https://quasar.dev/quasar-cli/developing-browser-extensions/content-hooks
 const FEED_CLASS = "css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu";
+const PANEL_CLASS = "css-1dbjc4n r-1ihkh82 r-1in3vh1 r-1867qdf r-1phboty r-rs99b7 r-1ifxtd0 r-1udh08x"
+
 const
   iFrame = document.createElement('iframe'),
   defaultFrameHeight = '100px',
   defaultFrameWidth = '120px'
-const currentUrl = window.document.URL
+
+  const currentUrl = window.document.URL
 console.log(currentUrl)
+
 export default function attachContentHooks (bridge) {
   // handle event
   bridge.on('focus', function (event) {
     console.log("focus mode")
     document.getElementsByClassName(FEED_CLASS)[1].style.visibility = "hidden"
+    document.getElementsByClassName(PANEL_CLASS)[0].style.visibility = "hidden"
   })
 
   bridge.on('un-focus', function (event) {
     console.log("unfocus mode")
     document.getElementsByClassName(FEED_CLASS)[1].style.visibility = "visible"
+    document.getElementsByClassName(PANEL_CLASS)[0].style.visibility = "visible"
   })
 
 
